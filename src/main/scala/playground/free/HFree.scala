335276032b44a033018be66e6ecceb1e714db6c7
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2021-08-11 12:29:47 +0300

    fuxes

--- src/main/scala/playground/free/HFree.scala
+++ src/main/scala/playground/free/HFree.scala
@@ -1,6 +1,6 @@
 package playground.free
 
-enum HFree[+U[+_[+_], +_], +A]
+enum HFree[+U[+_[+_], +_], +A]:
     case HPure[+A](a: A) extends HFree[Nothing, A]
     case HBind[+U[+_[+_], +_], A, +B](uf: U[HFree[U, *], A], f: A => HFree[U, B]) extends HFree[U, B]
 
--- src/main/scala/playground/typeclasses/catscheck.scala
+++
@@ -1,6 +0,0 @@
-package playground.typeclasses
-import cats.Parallel
-import cats.data.EitherT
-
-object Foo:
-    summon[Parallel[[A] =>> EitherT[List, String, A]]]

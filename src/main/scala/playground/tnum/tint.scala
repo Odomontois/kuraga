6458bc480882c0219ee5cdf482abfa2a2b04a350
Oleg Nizhnik <odomontois@gmail.com>
2021-09-29 17:16:17 +0300

    ----

--- src/main/scala/playground/tnum/tint.scala
+++ src/main/scala/playground/tnum/tint.scala
@@ -1,93 +1,52 @@
 package playground.tint
+import scala.compiletime.{constValueTuple, constValue}
 
-object Lol extends App {
+object Sums extends App:
+
+
+  println(constValueTuple[Plus[(true, true, true), (true, true)]]) // works
+  println(constValueTuple[Plus1[(true, true, true), (true, true)]]) // fails
+  println(constValueTuple[
+    Reverse[PlusLoop[Reverse[(true, true, true)], Reverse[(true, true)], false]]]
+    ) // also works despite it's just an unfold of `Plus1` application
+
+  type Plus[A <: Tuple, B <: Tuple] <: Tuple = (A, B) match
+    case (EmptyTuple, EmptyTuple) => EmptyTuple
+    case (a, b)                   => Reverse[PlusLoop[Reverse[A], Reverse[B], false]]
+
+  type Plus1[A <: Tuple, B <: Tuple]         = Reverse[PlusLoop[Reverse[A], Reverse[B], false]]
+
   type ReverseLoop[A, XS <: Tuple] <: Tuple = A match {
-    case Unit    => XS
-    case x *: xs => ReverseRecur[xs, x *: XS]
+    case EmptyTuple => XS
+    case x *: xs    => ReverseLoop[xs, x *: XS]
   }
-
-  type ReverseRecur[A, XS <: Tuple] = XS match { case ? => ReverseLoop[A, XS] }
 
   type Reverse[A] = ReverseLoop[A, EmptyTuple]
 
-  type PlusBit[A, B] = (A, B) match {
-    case (true, true)                  => (true, true)
-    case (true, false) | (false, true) => (false, true)
-    case (false, false)                => (false, false)
-  }
-
-  type PlusTri[A, B, C] <: (Boolean, Boolean) = (A, B, C) match {
+  type PlusTri[A, B, C]                             = (A, B, C) match
     case (false, false, false)                                              => (false, false)
     case (true, false, false) | (false, true, false) | (false, false, true) => (false, true)
     case (true, true, false) | (true, false, true) | (false, true, true)    => (true, false)
     case (true, true, true)                                                 => (true, true)
-  }
 
-  type Inc[A <: Tuple] <: Tuple = A match {
-    case Unit    => true *: Unit
-    case t *: as =>
-      t match {
+  type Inc[A <: Tuple] <: Tuple                     = A match
+    case EmptyTuple => true *: EmptyTuple
+    case t *: as    =>
+      t match
         case false => true *: as
         case true  => false *: Inc[as]
-      }
-  }
 
-  type IncT[A <: Tuple, O] <: Tuple = O match {
+  type IncT[A <: Tuple, O <: Boolean] <: Tuple      = O match
     case false => A
     case true  => Inc[A]
-  }
 
-  type PlusLoop[A <: Tuple, B <: Tuple, O] = (A, B) match {
-    case (Unit, Unit)       =>
-      O match {
-        case true  => (true *: Unit)
+  type PlusLoop[A <: Tuple, B <: Tuple, O] <: Tuple = (A, B) match
+    case (EmptyTuple, EmptyTuple) =>
+      O match
+        case true  => (true *: EmptyTuple)
         case false => EmptyTuple
-      }
-    case (Unit, ?)          => IncT[B, O]
-    case (?, Unit)          => IncT[A, O]
-    case (a *: as, b *: bs) =>
-      PlusTri[a, b, O] match {
-        case (o1, r) => r *: PlusLoop[as, bs, o1]
-      }
-  }
-
-  type Plus[A, B] = Reverse[PlusLoop[Reverse[A], Reverse[B], false]]
-
-  // inline def valueAll[XS <: Tuple] <: Tuple= (??? : XS) match {
-  //   case _ : Unit => ()
-  //   // case (_ : (x *: rest)) => implicit match {
-  //   //   case u : ValueOf[x] => u.value *: valueAll[rest]
-  //   // }
-  // }
-
-  type Fst[A <: Tuple] = A match {
-    case x *: y => x
-    case Unit   => Unit
-  }
-
-  trait ValueAll[T] {
-    def value: T
-  }
-
-  given ValueAll[EmptyTuple] with
-    def value = EmptyTuple
-  given [X, XS <: Tuple](using x: ValueOf[X], xs: ValueAll[XS]): ValueAll[X *: XS] with
-    def value = x.value *: xs.value
-
-  def valueAll[T <: Tuple](using v: ValueAll[T]): T = v.value
-  def printAll[T <: Tuple](using ValueAll[T])       = println(valueAll[T])
-
-  type R  = Plus[(true, true), (false, true)]
-  type R1 = PlusTri[true, false, true]
-
-  // printAll[PlusLoop[(true, true), (true, true), true]]
-
-  // println(valueAll[(1, 2, "kek", 4)])
-
-  type Q = Plus[(true, true, true), (true, true)]
-
-  // summon[Q =:= (true, false, true)]
-  // println(valueAll[Q])
-
-  // println(summon[ValueOf[R]].value)
-}
+    case (EmptyTuple, B)          => IncT[B, O]
+    case (A, EmptyTuple)          => IncT[A, O]
+    case (a *: as, b *: bs)       =>
+      PlusTri[a, b, O] match
+        case (x, y) => y *: PlusLoop[as, bs, x]

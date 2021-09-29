0cb3cd91f9ac0a8f7da2bcc74b9900dd4497bc08
Oleg Nizhnik <odomontois@gmail.com>
2021-09-29 12:35:31 +0300

    recover cherry tree

--- src/main/scala/playground/data/cherryTree.scala
+++ src/main/scala/playground/data/cherryTree.scala
@@ -1,59 +1,51 @@
-// package playground
+package playground
 
-// trait Monoid[A]:
-//     def empty: A
-//     extension (a: A) def |+| (b: A): A
+import kuraga.Monoid
+import kuraga.Semigroup.Sum
 
-// object Monoid:
-//     def empty[A](using m: Monoid[A]): A  = m.empty
+trait Measured[A, R]:
+  extension (a: A) def measure: R
 
-//     given Monoid[Int] with
-//         def empty = 0
-//         extension (x: Int) def |+| (y: Int) = x + y
+object Measured:
+  def of[A, R](a: A)(using Measured[A, R]): R = a.measure
 
-// trait Measured[A, R]:
-//     extension (a: A) def measure: R
+  given [A: Monoid]: Measured[A, A] with
+    extension (a: A) def measure: A = a
 
-// object Measured:
-//     def of [A, R](a: A)(using Measured[A, R]): R = a.measure
+  given [A, R](using Measured[A, R], Monoid[R]): Measured[(A, A), R] with
+    extension (a: (A, A)) def measure: R = a._1.measure |+| a._2.measure
 
-//     given [A]: Measured[A, A] with
-//         extension (a: A) def measure: A = a
+  given [A, R](using Measured[A, R], Monoid[R]): Measured[Option[A], R] with
+    extension (a: Option[A]) def measure: R = a.fold(Monoid.default)(_.measure)
 
-//     given [A, R](using Measured[A, R], Monoid[R]): Measured[(A, A), R] with
-//         extension (a: (A, A))  def measure: R = a._1.measure |+| a._2.measure
+enum CherryTree[A]:
+  case Empty()
+  case One(a: A)
+  case Branch(left: Option[A], mid: CherryTree[(A, A)], right: Option[A])
 
-//     given [A, R](using Measured[A, R], Monoid[R]): Measured[Option[A], R] with
-//        extension  (a: Option[A]) def measure: R = a.fold(Monoid.empty)(_.measure)
+  def :+(a: A): CherryTree[A] = this match
+    case Empty()               => One(a)
+    case One(b)                => Branch(None, One((b, a)), None)
+    case Branch(l, m, None)    => Branch(l, m, Some(a))
+    case Branch(l, m, Some(b)) => Branch(l, m :+ (b, a), None)
 
-// enum CherryTree[A]:
-//     case Empty()
-//     case One(a: A)
-//     case Branch(left: Option[A], mid: CherryTree[(A, A)], right: Option[A])
+  def +:(a: A): CherryTree[A] = this match
+    case Empty()               => One(a)
+    case One(b)                => Branch(None, One((a, b)), None)
+    case Branch(None, m, r)    => Branch(Some(a), m, r)
+    case Branch(Some(b), m, r) => Branch(None, (b, a) +: m, r)
 
-//     def :+(a: A): CherryTree[A] = this match
-//         case Empty()               => One(a)
-//         case One(b)                => Branch(None, One((b, a)), None)
-//         case Branch(l, m, None)    => Branch(l, m, Some(a))
-//         case Branch(l, m, Some(b)) => Branch(l, m :+ (b, a), None)
+object CherryTree:
+  def apply[A](a: A*): CherryTree[A] = a.foldLeft(CherryTree.Empty())(_ :+ _)
 
-//     def +:(a: A): CherryTree[A] = this match
-//         case Empty()               => One(a)
-//         case One(b)                => Branch(None, One((a, b)), None)
-//         case Branch(None, m, r)    => Branch(Some(a), m, r)
-//         case Branch(Some(b), m, r) => Branch(None, (b, a) +: m, r)
+  given [A, R](using Measured[A, R], Monoid[R]): Measured[CherryTree[A], R] with
+    extension (tree: CherryTree[A])
+      def measure = tree match
+        case CherryTree.Empty()         => Monoid.default
+        case CherryTree.One(a)          => a.measure
+        case CherryTree.Branch(l, m, r) => Measured.of(l) |+| m.measure |+| Measured.of(r)
 
-// object CherryTree:
-//     def apply[A](a: A*): CherryTree[A] = a.foldLeft(CherryTree.Empty())(_ :+ _)
-
-//     given measured[A, R](using Measured[A, R], Monoid[R]) as Measured[CherryTree[A], R]:
-//         def (tree: CherryTree[A]) measure = tree match
-//             case CherryTree.Empty()          => Monoid.empty
-//             case CherryTree.One(a)           => a.measure
-//             case CherryTree.Branch(l, m, r)  =>
-//                 Measured.of[R = R](l) |+| Measured.of[R = R](m) |+| Measured.of[R = R](r)
-
-// @main def cherryTreeMain() =
-//     val tree = CherryTree(1, 2, 3, 4, 5, 6, 7)
-//     println(tree)
-//     println(tree.measure)
+@main def cherryTreeMain() =
+    val tree = CherryTree[Sum[Int]](1, 2, 3, 4, 5, 6, 7)
+    println(tree)
+    println(tree.measure)

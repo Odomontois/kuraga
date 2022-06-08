6f230bfe9ee87bbe84f341539289ab4fcc63e4f8
Oleg Nizhnik <odomontois@gmail.com>
2022-06-08 10:58:09 +0400

    Star

--- src/main/scala/kuraga/datum/Star.scala
+++ src/main/scala/kuraga/datum/Star.scala
@@ -26,8 +26,8 @@ class Handler[+I[-_, +_], -O[-_, +_], +R](val values: Map[String, Proeff.Handler
   def ++[V[-_, +_], L[-_, +_], S](that: Handler[V, L, S]): Handler[I &:: V, O &:: L, R | S] =
     Handler(values ++ that.values)
   def andThen[T[-_, +_], S](that: Handler[O, T, S]): Handler[I, T, R | S]                   =
-    def mapFry(fry: Star[O, R]): Star[T, R | S] = Star.Elim[O, T, R | S](() => fry, that)
-    Handler(values.view.mapValues(_.map(mapFry)).toMap)
+    def mapstar(star: Star[O, R]) = Star.Elim[O, T, R | S](() => star, that)
+    Handler(values.view.mapValues(_.map(mapstar)).toMap)
 end Handler
 
 object Handle:
@@ -36,17 +36,18 @@ end Handle
 
 enum Star[-P[-_, +_], +R]:
   case Intro[Z, -P[-_, +_], +R](f: [A] => P[Star[P, R], A] => A)                               extends Star[P, R]
-  case Elim[Q[-_, +_], -P[-_, +_], +R](fry: () => Star[P &:: Q, R], handler: Handler[Q, P, R]) extends Star[P, R]
+  case Elim[Q[-_, +_], -P[-_, +_], +R](star: () => Star[P &:: Q, R], handler: Handler[Q, P, R]) extends Star[P, R]
   case Result(r: R)
 end Star
 
 object Star:
-  extension [R](fry: Star[AnyP, R]) def run: R = foldRun(fry)(Handle.Identity)
+  extension [R](star: Star[AnyP, R]) def run: R = foldRun(star)(Handle.Identity)
+  
 
-  def foldRun[P[-_, +_], S, R](fry: Star[P, S])(handler: Handler[P, AnyP, R]): S | R =
-    fry match
+  def foldRun[P[-_, +_], S, R](star: Star[P, S])(handler: Handler[P, AnyP, R]): S | R =
+    star match
       case Star.Intro(f)              => foldRun(f(handler.handle))(handler)
-      case Star.Elim(fryIn, handleIn) => foldRun(fryIn())(handler ++ handleIn.andThen(handler))
+      case Star.Elim(starIn, handleIn) => foldRun(starIn())(handler ++ handleIn.andThen(handler))
       case Star.Result(x)             => x
 end Star
 
